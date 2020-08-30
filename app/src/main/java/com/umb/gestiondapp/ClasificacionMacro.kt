e8c64171e3fd0b20ab0347bcea974ec7a681f60c
esgomezse <esgomezse@gmail.com>
2020-08-30 01:41:24 -0500

    Creación de clase clasificacion macro

---
+++ app/src/main/java/com/umb/gestiondapp/ClasificacionMacro.kt
@@ -0,0 +1,10 @@
+package com.umb.gestiondapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ClasificacionMacro:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/umb/gestiondapp/HomeActivity.kt
+++ app/src/main/java/com/umb/gestiondapp/HomeActivity.kt
@@ -1,10 +1,18 @@
 package com.umb.gestiondapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.home_activity.*
 
 class HomeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+        asistencias.setOnClickListener {
+            startActivity(
+                Intent(this,ClasificacionMacro::class.java)
+            )
+        }
     }
 }
