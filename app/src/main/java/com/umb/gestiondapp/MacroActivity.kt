0878be9967c7d42c73dfab5ebf3e8dc34c0cfd0d
jufarangoma <jufarangoma@gmail.com>
2020-09-20 00:59:42 -0500

    Set Database and spinner interaction

--- app/src/main/java/com/umb/gestiondapp/MacroActivity.kt
+++ app/src/main/java/com/umb/gestiondapp/MacroActivity.kt
@@ -1,32 +1,71 @@
 package com.umb.gestiondapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.inventario.*
 
-class MacroActivity: AppCompatActivity() {
-    val database = Firebase.database
-    val myRef = database.reference
+class MacroActivity : AppCompatActivity() {
+
+    private val database = Firebase.database
+    private var myRef: DatabaseReference = database.getReference("Activo")
+    private val micros = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.inventario)
-        myRef.child("Activo")
+        initListeners()
+    }
+
+    private fun initFirebaseListener() {
         myRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val value = dataSnapshot.getValue()
-                print(value)
+                micros.clear()
+                dataSnapshot.children.forEach {
+                    micros.add(it.key.toString())
+                }
+                setMicros()
             }
 
             override fun onCancelled(error: DatabaseError) {
-                // Failed to read value
+
                 print(error.toException())
             }
         })
+    }
+
+    private fun setMicros() {
+        spnMicro.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            micros
+        )
+    }
+
+    private fun initListeners() {
+        spnMacro.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                myRef = if (position == 0) database.getReference("Activo")
+                else database.getReference("Pasivo")
+                initFirebaseListener()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+
+            }
+        }
     }
 }
